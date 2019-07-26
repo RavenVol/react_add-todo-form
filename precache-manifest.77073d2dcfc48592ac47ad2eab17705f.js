e7d09aa04d20e46962420bb3befef8c28f9bb319
RavenVol <r.volianiuk@gmail.com>
2019-07-26 18:48:50 +0300

    Updates

---
+++ precache-manifest.77073d2dcfc48592ac47ad2eab17705f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a298a6e90b91ae5f1fcb87401da2f9f1",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "896c315ab477fbb7fe19",
+    "url": "/react_add-todo-form/static/css/main.a9bcce75.chunk.css"
+  },
+  {
+    "revision": "691c9819e7a112b7151f",
+    "url": "/react_add-todo-form/static/js/2.ed9456b3.chunk.js"
+  },
+  {
+    "revision": "896c315ab477fbb7fe19",
+    "url": "/react_add-todo-form/static/js/main.d2670078.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
